a06df4766b44f3863da0816c93814249809878b3
bibodha <bibodha.neupane@extendhealth.com>
2014-03-12 08:01:56 -0600

    Finished product

--- src/app.js
+++ src/app.js
@@ -2,7 +2,17 @@ define('app', ['jquery', 'underscore'], function($, _) {
   function App() {
     this.add = function(a, b) {
       return a + b;
-    }
+    };
+    
+    this.count = function(arr) {
+      return _.size(arr);
+    };
+
+    this.getEven = function(arr) {
+      return $(arr).filter(function(index) {
+        return arr[index] % 2 == 0;
+      }).toArray();
+    };
   }
   return new App();
 });
--- test/appSpec.js
+++ test/appSpec.js
@@ -4,6 +4,66 @@ define(['app', 'jquery', 'underscore'], function (app, $, _) {
         // just checking that _ works
       expect(app).toBeDefined();
     });
+
+    it('add should return 3', function() {
+      expect(app.add(1,2)).toEqual(3);
+    });
+
+    it('count should return 4', function() {
+      expect(app.count([0,1,2,3])).toEqual(4);
+    });
+
+    it('should return even', function () {
+      var arr = [0,1,2,3,4];
+      var newArr = app.getEven(arr);
+
+      expect(newArr).toContain(2);
+      expect(newArr).toNotContain(3);
+    });
+  });
+  describe('just checking', function() {
+    it('app should be defined', function() {
+        // just checking that _ works
+      expect(app).toBeDefined();
+    });
+
+    it('add should return 3', function() {
+      expect(app.add(1,2)).toEqual(3);
+    });
+
+    it('count should return 4', function() {
+      expect(app.count([0,1,2,3])).toEqual(4);
+    });
+
+    it('should return even', function () {
+      var arr = [0,1,2,3,4];
+      var newArr = app.getEven(arr);
+
+      expect(newArr).toContain(2);
+      expect(newArr).toNotContain(3);
+    });
+  });
+  describe('just checking', function() {
+    it('app should be defined', function() {
+        // just checking that _ works
+      expect(app).toBeDefined();
+    });
+
+    it('add should return 3', function() {
+      expect(app.add(1,2)).toEqual(3);
+    });
+
+    it('count should return 4', function() {
+      expect(app.count([0,1,2,3])).toEqual(4);
+    });
+
+    it('should return even', function () {
+      var arr = [0,1,2,3,4];
+      var newArr = app.getEven(arr);
+
+      expect(newArr).toContain(2);
+      expect(newArr).toNotContain(3);
+    });
   });
 });
 
